d25a4a5c31f12054b60f7595e0de0132d5f146e3
tcandens <joey.thies@gmail.com>
2015-03-13 11:08:26 -0700

    Working headroom.js and scrollReveal.js Navigation bar uses transformZ(0) hack to fix scrolling repaint jitter

---
+++ src/assets/javascript/main.js
@@ -0,0 +1,13 @@
+$(function() {
+  var el = document.querySelector('nav');
+  var headroom = new Headroom(el);
+  headroom.init();
+
+  window.sr = new scrollReveal({
+    enter:  'bottom',
+    move:   '200px',
+    over:   '500ms',
+    easing: 'ease-out'
+  });
+
+})();
